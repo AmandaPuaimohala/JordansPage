c73dee5e606941578945ac881bb1dece8e9f8398
Amanda <anpt717@gmail.com>
2026-01-08 08:37:58 -1000

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
  * @type {import('vite').UserConfig}
  */
 export default {
-    base: process.env.NODE_ENV === 'production'?'/JordansPage/' : ''
+    base: process.env.NODE_ENV === 'production' ? '/JordansPage/' : ''
 }
